53c7fb9e69a93dc3cedaaee700762a7e8b0e8d30
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-10-14 17:45:51 +0545

    init commit

---
+++ tania-tutorial/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, React!</h1>
+      </div>
+    )
+  }
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
